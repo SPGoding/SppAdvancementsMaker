19e36194248a8d5375b822d33f7cce7e2ca2d514
SpaceBang <SpaceBang@foxmail.com>
2017-07-08 22:53:03 +0800

    Added ZhEnBlocks. QwQ

---
+++ SppAdvancementsMaker/TriggerEditors/FormEnterBlock.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormEnterBlock
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBoxRecipe = New System.Windows.Forms.TextBox()
+        Me.ButtonEnter = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(16, 150)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(7, 0, 7, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(135, 20)
+        Me.Label3.TabIndex = 56
+        Me.Label3.Text = "大多数条件是可选的"
+        '
+        'TextBoxRecipe
+        '
+        Me.TextBoxRecipe.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBoxRecipe.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.TextBoxRecipe.Location = New System.Drawing.Point(87, 41)
+        Me.TextBoxRecipe.Name = "TextBoxRecipe"
+        Me.TextBoxRecipe.Size = New System.Drawing.Size(243, 26)
+        Me.TextBoxRecipe.TabIndex = 55
+        '
+        'ButtonEnter
+        '
+        Me.ButtonEnter.Location = New System.Drawing.Point(212, 150)
+        Me.ButtonEnter.Name = "ButtonEnter"
+        Me.ButtonEnter.Size = New System.Drawing.Size(118, 45)
+        Me.ButtonEnter.TabIndex = 54
+        Me.ButtonEnter.Text = "确定"
+        Me.ButtonEnter.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 44)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(7, 0, 7, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 20)
+        Me.Label2.TabIndex = 53
+        Me.Label2.Text = "方块ID："
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 9)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(7, 0, 7, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(205, 20)
+        Me.Label1.TabIndex = 52
+        Me.Label1.Text = "本触发器在玩家进入方块时触发"
+        '
+        'FormEnterBlock
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(345, 207)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBoxRecipe)
+        Me.Controls.Add(Me.ButtonEnter)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("微软雅黑", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "FormEnterBlock"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "#2进度修改者_条件编辑器"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBoxRecipe As TextBox
+    Friend WithEvents ButtonEnter As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
